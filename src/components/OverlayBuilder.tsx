98ddf89a685f158a0865ffc00e7300d2f71456fe
Alex Motoc <alexmotoc97@gmail.com>
2019-11-21 19:59:17 +0000

    Added TextField that updates the text inside the preview

--- src/components/OverlayBuilder.tsx
+++ src/components/OverlayBuilder.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
+import TextField from '@material-ui/core/TextField';
 import { OverlayDimension } from './OverlayDimension';
 import { OverlayPreview } from './OverlayPreview';
 
 export const OverlayBuilder: React.FunctionComponent<{}> = () => {
     const [width, setWidth] = React.useState(0);
     const [height, setHeight] = React.useState(0);
+    const [text, setText] = React.useState('');
 
     const handleDimensionSelect = (dimension: string, value: number) => {
         if (dimension === "Width") {
@@ -14,11 +16,22 @@ export const OverlayBuilder: React.FunctionComponent<{}> = () => {
         }
     };
 
+    const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setText(event.target.value);
+    };
+
     return (
         <React.Fragment>
             <OverlayDimension name="Height" onDimensionSelect={handleDimensionSelect}/>
             <OverlayDimension name="Width" onDimensionSelect={handleDimensionSelect}/>
-            <OverlayPreview width={width} height={height}/>
+            <TextField 
+                label="Text"
+                variant="outlined"
+                onChange={handleTextChange} />
+            <OverlayPreview 
+                width={width} 
+                height={height}
+                text={text} />
         </React.Fragment>
     );
 }
