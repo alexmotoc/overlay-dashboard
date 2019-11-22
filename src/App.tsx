bc3bb724cf78305570268c817acfd41f022489d1
Alex Motoc <alexmotoc97@gmail.com>
2019-11-22 08:46:07 +0000

    Added colour scheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import './App.css';
 import { OverlayBuilder } from './components/OverlayBuilder';
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import cyan from '@material-ui/core/colors/cyan';
+import deepOrange from '@material-ui/core/colors/deepOrange';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: cyan[300],
+      main: cyan[500],
+      dark: cyan[700],
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: deepOrange[100],
+      main: deepOrange[300],
+      dark: deepOrange[500],
+      contrastText: '#000',
+    },
+  },
+});
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <OverlayBuilder />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <OverlayBuilder />
+      </div>      
+    </ThemeProvider>
   );
 }
 
