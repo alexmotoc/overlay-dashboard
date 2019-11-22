9783c7b200c0e7b703cafe09278cbb13f48381c7
Alex Motoc <alexmotoc97@gmail.com>
2019-11-22 13:30:42 +0000

    Added dialogs to include each individual color picker

--- src/components/OverlayBuilder.tsx
+++ src/components/OverlayBuilder.tsx
@@ -1,17 +1,28 @@
 import * as React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import { OverlayDimension } from './OverlayDimension';
 import { OverlayPreview } from './OverlayPreview';
 import { SketchPicker, ColorResult } from 'react-color';
 import { Typography } from '@material-ui/core';
+import PaletteIcon from '@material-ui/icons/Palette';
 
 export const OverlayBuilder: React.FunctionComponent<{}> = () => {
+    const white: string = '#fff';
+    const black: string = '#000';
+    
     const [width, setWidth] = React.useState(0);
     const [height, setHeight] = React.useState(0);
     const [text, setText] = React.useState('');
-    const [textColor, setTextColor] = React.useState('#fff');
-    const [overlayColor, setOverlayColor] = React.useState('#000');
+    const [textColor, setTextColor] = React.useState(white);
+    const [isTextColorOpen, setIsTextColorOpen] = React.useState(false);
+    const [overlayColor, setOverlayColor] = React.useState(black);
+    const [isOverlayColorOpen, setIsOverlayColorOpen] = React.useState(false);
 
     const handleDimensionSelect = (dimension: string, value: number) => {
         if (dimension === "Width") {
@@ -24,14 +35,42 @@ export const OverlayBuilder: React.FunctionComponent<{}> = () => {
     const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setText(event.target.value);
     };
+
+    const handleTextColorPickerClick = () => {
+        setIsTextColorOpen(true);
+    };
+
+    const handleCancelTextColor = () => {
+        // Hide dialog and reset colour of text to default option
+        setIsTextColorOpen(false);
+        setTextColor(white);
+    };
+
+    const handleConfirmTextColor = () => {
+        setIsTextColorOpen(false);
+    };
     
     const handleTextColorChange = (color: ColorResult) => {
         setTextColor(color.hex);
-    }
+    };
+
+    const handleOverlayColorPickerClick = () => {
+        setIsOverlayColorOpen(true);
+    };
+
+    const handleCancelOverlayColor = () => {
+        // Hide dialog and reset colour of overlay to default option
+        setIsOverlayColorOpen(false);
+        setOverlayColor(black);
+    };
+
+    const handleConfirmOverlayColor = () => {
+        setIsOverlayColorOpen(false);
+    };
 
     const handleOverlayColorChange = (color: ColorResult) => {
         setOverlayColor(color.hex);
-    }
+    };
 
     return (
         <React.Fragment>
@@ -42,9 +81,27 @@ export const OverlayBuilder: React.FunctionComponent<{}> = () => {
                     </Typography>
                     <OverlayDimension name='Height' onDimensionSelect={handleDimensionSelect}/>
                     <OverlayDimension name='Width' onDimensionSelect={handleDimensionSelect}/>
-                    <SketchPicker 
-                        color={overlayColor}
-                        onChangeComplete={handleOverlayColorChange}/>
+                    <Button
+                        onClick={handleOverlayColorPickerClick}>
+                        <PaletteIcon color='primary' />
+                    </Button>
+                    <Dialog
+                        open={isOverlayColorOpen} >
+                        <DialogTitle>Overlay Colour</DialogTitle>
+                        <DialogContent dividers>
+                            <SketchPicker 
+                                color={overlayColor}
+                                onChangeComplete={handleOverlayColorChange}/>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button autoFocus onClick={handleCancelOverlayColor} color="primary">
+                            Cancel
+                            </Button>
+                            <Button onClick={handleConfirmOverlayColor} color="primary">
+                            OK
+                            </Button>
+                        </DialogActions>
+                    </Dialog>                   
                     <Typography variant='h5'>
                         Text
                     </Typography>
@@ -52,9 +109,27 @@ export const OverlayBuilder: React.FunctionComponent<{}> = () => {
                         disabled={width === 0 || height === 0}
                         variant='outlined'
                         onChange={handleTextChange} />
-                    <SketchPicker 
-                        color={textColor}
-                        onChangeComplete={handleTextColorChange}/>
+                    <Button
+                        onClick={handleTextColorPickerClick}>
+                        <PaletteIcon color='primary' />
+                    </Button>
+                    <Dialog
+                        open={isTextColorOpen} >
+                        <DialogTitle>Text Colour</DialogTitle>
+                        <DialogContent dividers>
+                            <SketchPicker 
+                                color={textColor}
+                                onChangeComplete={handleTextColorChange}/>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button autoFocus onClick={handleCancelTextColor} color="primary">
+                            Cancel
+                            </Button>
+                            <Button onClick={handleConfirmTextColor} color="primary">
+                            OK
+                            </Button>
+                        </DialogActions>
+                    </Dialog>  
                 </Grid>
                 <Grid item xs>
                     <OverlayPreview 
