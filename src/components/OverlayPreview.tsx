219884382b33d0f373ee3a99f975f3d6da517c4f
Alex Motoc <alexmotoc97@gmail.com>
2019-11-22 10:51:18 +0000

    Improved layout, adjusted overlay box to work with percentages and disabled text field if either dimension is 0

--- src/components/OverlayPreview.tsx
+++ src/components/OverlayPreview.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import grey from '@material-ui/core/colors/grey';
 
 const useStyles = makeStyles({
+    preview: {
+        width: '75%',
+        height: '30%',
+        backgroundColor: grey[300]
+    },
     overlay: {
-      width: (props: OverlayPreviewProps) => props.width,
-      height: (props: OverlayPreviewProps) => props.height,
+      width: (props: OverlayPreviewProps) => `${props.width}%`,
+      height: (props: OverlayPreviewProps) => `${props.height}%`,
       backgroundColor: (props: OverlayPreviewProps) => props.overlayColor,
-      textAlign: 'center'
+      textAlign: 'center',
+      overflow: 'hidden'
     },
     text: {
         display: 'block',
@@ -29,8 +37,15 @@ export const OverlayPreview: React.FunctionComponent<OverlayPreviewProps> = (pro
     const classes = useStyles(props);
     
     return (
-        <div className={classes.overlay}> 
-            <span className={classes.text}>{props.text}</span>         
-        </div>
+        <React.Fragment>
+            <Typography variant='h3'>
+                Overlay Preview
+            </Typography>
+            <div className={classes.preview}>
+                <div className={classes.overlay}> 
+                    <span className={classes.text}>{props.text}</span>         
+                </div>            
+            </div>
+        </React.Fragment>
     );
 }
