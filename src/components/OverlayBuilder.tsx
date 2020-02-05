95ad9bfa24df24c28e716cdcbc134f151cd9640e
Alex Motoc <alexmotoc97@gmail.com>
2020-02-05 18:54:46 +0000

    Added ability to save overlay template

--- src/components/OverlayBuilder.tsx
+++ src/components/OverlayBuilder.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
+import axios from 'axios';
+import Fab from '@material-ui/core/Fab';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
+import SaveIcon from '@material-ui/icons/Save';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
 import { makeStyles } from '@material-ui/core/styles';
 import { ColorPicker } from './ColorPicker';
 import { OverlaySlider } from './OverlaySlider';
@@ -9,7 +14,6 @@ import { TextStyle, TextFormats } from './TextStyle';
 import { RGBColor } from 'react-color';
 import { Typography } from '@material-ui/core';
 import { TextAlignment } from './TextStyle';
-import { fontWeight } from '@material-ui/system';
 
 export const streamWidth: number = 1920;
 export const streamHeight: number = 1080;
@@ -17,6 +21,10 @@ export const streamHeight: number = 1080;
 export const white: RGBColor = {r: 255, g: 255, b: 255, a: 1.0};
 export const black: RGBColor = {r: 0, g: 0, b: 0, a: 1.0};
 
+const Alert = (props: AlertProps) => {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+  
 const useStyles = makeStyles({
     form: {
         marginLeft: 50
@@ -39,6 +47,12 @@ const useStyles = makeStyles({
     },
     textStyle: {
         display: 'flex'
+    },
+    extendedIcon: {
+        marginRight: 10,
+    },
+    saveButton: {
+        marginTop: 15
     }
 });
 
@@ -54,6 +68,7 @@ export const OverlayBuilder: React.FunctionComponent<{}> = () => {
     const [textFormats, setTextFormats] = React.useState(['' as TextFormats]);
     const [textColor, setTextColor] = React.useState(white);
     const [overlayColor, setOverlayColor] = React.useState(black);
+    const [isToastOpen, setIsToastOpen] = React.useState(false);
 
     const handleValueChange = (attribute: string, value: number) => {
         switch (attribute.toLowerCase()) {
@@ -75,6 +90,34 @@ export const OverlayBuilder: React.FunctionComponent<{}> = () => {
     const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setText(event.target.value);
     };
+
+    const handleSaveOverlay = () => {
+        const data = {
+            'size': {
+                'width': width,
+                'height': height
+            },
+            'position': {
+                'horizontal': horizontal,
+                'vertical': vertical
+            },
+            'text': {
+                'content': text,
+                'alignment': textAlignment,
+                'format': textFormats,
+                'colour': textColor
+            },
+            'colour': overlayColor
+        }
+
+        axios.post('http://127.0.0.1:8000/overlays/', {'template': JSON.stringify(data)}).then(response => {
+            setIsToastOpen(true);
+        });
+    }
+
+    const handleCloseToast = () => {
+        setIsToastOpen(false);
+    }
 
     return (
         <React.Fragment>
@@ -139,6 +182,10 @@ export const OverlayBuilder: React.FunctionComponent<{}> = () => {
                             textColorCallback={setTextColor}
                             textFormatsCallback={setTextFormats}/>
                     </div>
+                    <Fab className={classes.saveButton} color="primary" variant="extended" onClick={handleSaveOverlay}>
+                        <SaveIcon className={classes.extendedIcon}/>
+                        Save
+                    </Fab>
                 </Grid>
                 <Grid item xs className={classes.previewArea}>
                     <OverlayPreview 
@@ -153,6 +200,11 @@ export const OverlayBuilder: React.FunctionComponent<{}> = () => {
                         textFormats={textFormats} />
                 </Grid>
             </Grid>
+            <Snackbar open={isToastOpen} autoHideDuration={1500} onClose={handleCloseToast}>
+                <Alert onClose={handleCloseToast} severity="success">
+                Overlay was saved successfully!!
+                </Alert>
+            </Snackbar>
         </React.Fragment>
     );
 }
